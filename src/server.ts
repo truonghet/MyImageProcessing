390f83cc04371d87df978d487586cd39b220303b
Het Truong <truongvanhetln@gmail.com>
2024-04-02 18:00:21 +0700

    refactor + fix bug

---
+++ src/server.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+(async () => {
+
+  const app = express();
+
+  const port = process.env.PORT || 8082;
+  
+  app.use(bodyParser.json());
+
+  app.get( "/filteredimage", async(req:express.Request, res:express.Response) => {
+    let {image_url} = req.query;
+    if (!image_url){
+      res.status(400).send('Error: The submitted url is empty');
+    } else {
+      await filterImageFromURL(image_url).then( function (image_filtered_path){
+        res.sendFile(image_filtered_path, () => {       
+          deleteLocalFiles([image_filtered_path]);       
+        });   
+      }).catch(function(err){
+        res.status(400).send('Error:' + err);
+      });  
+
+    }
+  });
+  
+  app.get( "/", async(req:express.Request, res:express.Response) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
---
+++ www/server.js
@@ -0,0 +1,44 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const util_1 = require("./util/util");
+(() => __awaiter(this, void 0, void 0, function* () {
+    const app = express_1.default();
+    const port = process.env.PORT || 8082;
+    app.use(body_parser_1.default.json());
+    app.get("/filteredimage", (req, res) => __awaiter(this, void 0, void 0, function* () {
+        let { image_url } = req.query;
+        if (!image_url) {
+            res.status(400).send('Error: The submitted url is empty');
+        }
+        else {
+            yield util_1.filterImageFromURL(image_url).then(function (image_filtered_path) {
+                res.sendFile(image_filtered_path, () => {
+                    util_1.deleteLocalFiles([image_filtered_path]);
+                });
+            }).catch(function (err) {
+                res.status(400).send('Error:' + err + 'For some reason the image cannot be filtered. Please provide the following ID to our support:' + Math.random().toString(36).substr(2, 9));
+            });
+        }
+    }));
+    app.get("/", (req, res) => __awaiter(this, void 0, void 0, function* () {
+        res.send("try GET /filteredimage?image_url={{}}");
+    }));
+    app.listen(port, () => {
+        console.log(`server running http://localhost:${port}`);
+        console.log(`press CTRL+C to stop server`);
+    });
+}))();
+//# sourceMappingURL=server.js.map
