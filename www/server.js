e9b8e5eea133b8cc63de40f2a3874b86e6bf0308
Het Truong <truongvanhetln@gmail.com>
2024-04-06 10:33:24 +0700

    go now

--- www/server.js
+++ www/server.js
@@ -1,9 +1,10 @@
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
@@ -14,14 +15,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const util_1 = require("./util/util");
-(() => __awaiter(this, void 0, void 0, function* () {
+(() => __awaiter(void 0, void 0, void 0, function* () {
     const app = express_1.default();
     const port = process.env.PORT || 8082;
     app.use(body_parser_1.default.json());
-    app.get("/filteredimage", (req, res) => __awaiter(this, void 0, void 0, function* () {
-        let { image_url } = req.query;
+    app.get("/filteredimage", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+        const { image_url } = req.query;
         if (!image_url) {
-            res.status(400).send('Error: The image url cannot be empty!');
+            res.status(400).send('The image url cannot be empty!');
         }
         else {
             yield util_1.filterImageFromURL(image_url).then(function (image_filtered_path) {
@@ -29,11 +30,11 @@ const util_1 = require("./util/util");
                     util_1.deleteLocalFiles([image_filtered_path]);
                 });
             }).catch(function (err) {
-                res.status(400).send(err);
+                res.status(400).send('Please make sure that the image url is valid!');
             });
         }
     }));
-    app.get("/", (req, res) => __awaiter(this, void 0, void 0, function* () {
+    app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         res.send("try GET /filteredimage?image_url={{}}");
     }));
     app.listen(port, () => {
